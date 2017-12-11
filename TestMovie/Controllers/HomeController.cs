f573576b3105bc10cbb39b199ba75d201b679602
Sapphire <thapasailuja123@gmail.com>
2017-12-11 17:42:07 -0600

    Movie Database Version 1.0

---
+++ TestMovie/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMovie.Models;
+
+namespace TestMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Login to add or edit movies ";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Registration obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                MoviesDBEntities db = new MoviesDBEntities();
+                db.Registrations.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Login (TestMovie.Models.Registration user)
+        {
+            using (MoviesDBEntities db = new MoviesDBEntities())
+            {
+                {
+                    var userDetails = db.Registrations.Where(X => X.FirstName == user.FirstName && X.Password == user.Password).FirstOrDefault();
+                    if (userDetails == null)
+                    {
+                        user.LoginErrorMessage = "Invalid User Name or Password";
+                        return View("Login", user);
+                    }
+                    else
+                    {
+                        Session["userID"] = userDetails.UserID;
+                        Session["UserName"] = userDetails.UserName;
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+            }
+            
+                
+        }
+        public ActionResult LogOut()
+        {
+            int userID = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ TestMovie/Controllers/moviesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestMovie.Models;
+
+namespace TestMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MoviesDBEntities1 db = new MoviesDBEntities1();
+
+        // GET: movies
+        //public ActionResult Index()
+        //{
+        //    return View(db.movies.ToList());
+        //}
+
+        public ActionResult Index(string movieGenre, string searchString)
+        {
+            var GenreList = new List<string>();
+            var GenreQuery = from d in db.movies
+                            orderby d.Genre
+                            select d.Genre;
+            GenreList.AddRange(GenreQuery.Distinct());
+            ViewBag.moviegenre = new SelectList(GenreList);
+
+            var movies = from m in db.movies
+                         select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            return View(movies);
+        }
+
+        // GET: movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            movy movy = db.movies.Find(id);
+            if (movy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movy);
+        }
+
+        // GET: movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: movies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Genre,Rating,Actor,Director")] movy movy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.movies.Add(movy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movy);
+        }
+
+        // GET: movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            movy movy = db.movies.Find(id);
+            if (movy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movy);
+        }
+
+        // POST: movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Genre,Rating,Actor,Director")] movy movy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movy);
+        }
+
+        // GET: movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            movy movy = db.movies.Find(id);
+            if (movy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movy);
+        }
+
+        // POST: movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            movy movy = db.movies.Find(id);
+            db.movies.Remove(movy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
